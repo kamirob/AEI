5919a423da8f130cef62f5851fbabcc66d43a9db
kamirob <camilobastidas1996@gmail.com>
2019-05-31 14:46:31 -0500

    Save before install gem administrate

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get :generate_pdf, to: "products#generate_pdf"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  resources :dashboard
 end
