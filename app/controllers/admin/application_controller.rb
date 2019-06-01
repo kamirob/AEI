946588c88b76e58aaddf1d15ded60ef2fe820d9e
kamirob <camilobastidas1996@gmail.com>
2019-06-01 15:05:49 -0500

    Validation user to admin page

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,11 +6,12 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
+    before_action :authenticate_user!
     before_action :set_locale
+    before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+        
     end
 
     private
